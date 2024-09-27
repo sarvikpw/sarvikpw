0f6468121f3613f768c002f9b1f5ed08340c936c
sarvikpw <sarvik.professional@gmail.com>
2024-09-27 12:41:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+const group201 = [
+    "Prableen Kaur", "Prakhar Pandey", "Mukesh Singh", "Himanshu Kushwaha", "Abhay Pandey", "Khushboo Goyal", "Adarsh Bahuguna", "Rounak Ray", "Abhirupa", "Conrad", "Anirudha Debnath", "Divyansh Chaudhary", "Aroshi Biswas", "Arnav", "Arika Khareta", "Rahul Ghosh", "Anshika Shaw", "Pratik Singh", "Ekayan", "Ishant Gupta", "Aniketa Mondal", "Md Muneer", "Hardik Kumar", "Tanishque Shukla", "Albert Singha", "Aditya", "Sanjana", "Yash Shukla", "Riddhima", "Neelakshi", "Tathagat Kumar", "Apurva Sharma", "Ubaid", "Savitri", "Chaitanya Pawar", "Sakshi", "Bhumika", "Prince Kumar", "Madhu Sudhan Rastogi", "Aastha Bramhankar", "Srijan Halder", "Shagun Sehra", "Yashika", "Madara Uchiha", "Utkarsh Tripathi", "Sriyank Raj", "Saloni Mishra", "Ayush Lahane", "Shiva Singh", "Lovely Kumari", "Surya Narayan", "Harshita", "Trishna Das", "Kanika Kanwar", "Antara Sinha", "Aryan Anand", "Satyam Singh", "Arav Singh", "Mahin Choudhury", "Ayush Shukla", "CEO of Karnauti", "Kushal Maurya", "Nishu Singh", "Pratyaksh", "Prinsh Kumar", "Yakshita", "Nick", "Pranjal", "Pranjal Vyas", "Kartikey Gautam Singh", "Darshit Verma", "Arinav Sahoo", "Rashi Meher", "Dhanshree Turkar", "Prachi", "Madhav Jain", "Pranjay Kumar Singh", "Chandra Prakash Singh Rajput", "Ankita Das", "Amritansh Barnwal", "Prachi Chavan", "Pranav Parashar", "Piyush Sinha", "Sarvesh Deshmukh", "Saurav Kumar", "Priyansh Anand", "Amritanshu Rana", "Pravin Khare", "Nancy Singh", "Harshal Bharat Mali", "Manisha Nayak", "Nupul Patil", "Hardik Lal Gadri", "Naman Chhabra", "Chitransh", "Snehangshu Das", "Rishi Patel", "Pradeep Choudhary", "Shounak", "Aditya Pandey", "Sidharth Jaat", "Saksham Bansal", "Yashass", "Himanshu Yadav", "Rishabh Rajak", "Bhomik Singh", "Manya Washkhiyar", "Rushil Jain", "Akhilesh Bhagat", "Ishan Jain", "Harsh Jain", "Omprakash Parida", "Nishant Kumar Sinha", "Ansh", "Dharmik", "Utsha Bhattacharyya", "Revansh Sharma", "Subhashree Priyadarshinee Patra", "Amanpreet", "Rounak Kumar", "Parambrata Dutta", "Anshu Goyal", "Tanmay Tripathi", "Hetain Arora", "Saksham Verma", "Mayur Kumavat", "Shuvadeep Das", "Lalit Kumar", "Akshay Verma", "Yuvraj Tiwari", "Navya", "Nabha Joshi", "Harshita Sharma"
+];
+const group202 = [
+    "Mission Iit", "Harmanjot Kaur", "Vaibhavi", "Satyarth Pandey", "Piyush Kumar", "Abika Sahu", "Samruddhi Gomase", "Kavy", "Vaishnavi", "Ananya Singh", "Patel Amisha", "Kushagra Singh", "Gargi Chakraborty", "Arav Mathur", "Anirban Das", "Sourav Kumar Sahoo", "Goutam Kumar Mandal", "Soudip Seth", "Shreeyans Sharma", "Aryan Raj Das", "Ansh", "Krititwa Kaushik", "Sara Kashyap", "Vaibhav Singh", "Archisman Das", "Saksham Dhiman", "Nitesh Kumar", "Kanishk Sharma", "Prince Yadav", "Sanidhya Mehta", "Aastik CD", "Arman Ali", "Harshit Saxena", "Ayoush Singh", "Soumya", "Diya", "Paras Kaushal", "Mihir Haldar", "Parth", "Aditya Chauhan", "Madhurrr Joshi", "Sarang Shahare", "Abhinav", "Ishmeet Bhogal", "Sarvesh Kanakdande", "Anmol Nath", "Atharva Nande", "Mitansh", "Awani Dikshit", "Soham Das Sharma", "Afran Khan", "Priyanshu Prajapat", "Ashish Das", "Sahil Maheshwari", "R Pranaam Mithra", "Krishna Tripathi", "Vickey", "M D Saif", "Rashi Rana", "Kartik Mittal", "Mayank Raj", "Mihir Shukla", "Bhoomika Choudhary", "Ayush Yadav", "Aradhana Narang", "Arnav Bajpai", "Vaishnavi Tyagi", "Mayank Tripathi", "Gursimar Singh", "Sumit Mishra", "Mohd Aqil", "Krrish Kumar", "Prince", "Shivam Yadav", "Abhinay Sharma", "Hitesh Goyal", "Abhay Yadav", "Shikha Mahto", "Harshit Kumar Tiwari", "Piyush Tyagi", "Vaibhav Tilak", "Kushagra Kartavya", "Rishabh Adeshra", "Rajan Agarwal", "Advik Mishra", "Rambhadra Pandey", "Neeraj Kumar Sahu", "Aryan", "Sai Pallavi", "Manas Tiwari", "Tanubhav Bhagowati", "Maitri Gujrati", "Ashish Patel", "Yash Shukla", "Palak Yadav", "Poojith", "Ritesh Kumar", "Krutika", "Ashutosh Deep", "Vyom Sawla", "Suryansh Sigma", "Keshav Patel", "Prapti Bajpayee", "Dibyanshu Shekhar", "Srishti Gupta", "Bhavesh Upadhyay", "Tanvi", "Sunny Nath", "Dream World", "Samarth Agnihotri", "Manasvi Goyal", "Rudra Gupta", "Suleksha Yadav", "Yatharth Relan", "Samiksha Gayake", "Harleen", "Pritish", "Priyancee Srivastava", "Astha Dubey", "Kartik", "Nikhil", "Ujjwal Saini"
+];
+const group203 = [
+    "Swarnava Datta Mondal", "Raj Singhal", "Shreya Das", "Shristi Bhatt", "Anuttam Behera", "Bhavya Jaiswal", "Vaibhav S", "Sumit Rajput", "Nitin Yadav", "Vyankatesh Dabhade", "Harsh Jindal", "Krishna Prajapati", "Prachi", "Prakarsh Solanki", "Aditya Aggarwal", "Lina Patil", "Aditya Pandey", "Durga Kumari", "Kalrav Srivastava", "Ranjan", "Manaswini Ch", "Aman Deep Singh", "Jonak Adipta Kalita", "Prathamesh More", "Srikant Maurya", "Atul Anand", "Anisha Debnath", "Aviral Joshi", "Divyanka Sharma", "X Pal", "Sahil", "Ayush", "Piyush", "Satyam", "Uzma Khan", "Samruddhi", "Divyansh", "Harsh Vardhan Singh Gariya", "Ankit Rajj", "Iitkre Liye Kuch Bhi Karega", "Bhavika", "Iit Bombay", "Rohit Raj", "Sheetal Singh", "Pratiush Yadav", "Jaya", "Shivam Verma", "Nivedita Shivaji Kuwar", "Gauri Santosh Thombare", "Abhigyan Dutta", "Akash Yadav", "Debarpan Pal", "Harsh Srivastava", "Srishti Singh", "Bablu Kumar", "Anish Singh", "Shaurya Pratap Singh", "Arpit Behera", "Nayan Jii", "Somesh", "Anuj Yadav", "Prashant Lodhi", "Kavya Shah", "Shadman Ansari", "Prabal Sharma", "Prakash Anand Singh Jeena", "Samadrito Malakar", "Avika Garg", "Udit Sharma", "Tanya Singh", "Swarali Bhosale", "Shaurya Jain", "Sagnik Roy", "Rajen Guha", "Lakshya Gupta", "Arush Arora", "Ripsita Ghosh", "Kushagra Sharma", "Krish Rajput", "Nitin", "Kunal Sharma", "Bhargav Anand", "Anvi Chauhan", "Ambika Jha", "Rudraaadip Mondal", "Sumant Singh", "Nikita", "Sanskar Tiwari", "Ks Guru Krupa", "Saharsh Mishra", "Nobita", "Sarthak Sanise", "Hritick Pal", "Anshijain Jain", "Saptak Kumar", "Yuvraj Singh", "Sugato Biswas", "Aditi Shrivastava", "Puneet Goyal", "Tanishq Maan", "Gungun Keshwani", "Aniket", "Anshika Malik", "Pratyush Mishra", "Harsh Jaiswal", "Harshit Kumar", "Vaibhav Kumar", "Tushar Kumar", "Mehakpreet Kaur", "Shorya Agrawal", "Parth Shukla", "Rajat Rajoria", "Harshit", "Digvijay Singh Airee", "Utkarsh Lote", "Venkatesh", "Girish Bhati", "Hitesh Ahuja", "Aniket Srivastava", "Shanu Kumar", "Avnish Raj", "Aadarsh Shekhar", "Pranjal Gupta", "Saachi Varu", "Himanshu Chaudhary", "Arpit", "Nandini", "Sohil Ansari", "Rajnish", "Tanishq Tiwari", "Krishnansh Yadav", "Stuti Mashalkar", "Divyanshi", "Sattwik Das", "Dhruv Chaudhary"
+];
+const group204 = [
+    "Sanskar", "Shlok Gadwale", "Prince Kumar", "Divyanshi Negi", "Ojas Tripathi", "Nishchaye Parihar", "Prachi Jha", "Somneel Das", "Suryansh Dubey", "Pallavi Kushwaha", "Ritesh Shukla", "Aditya Banerjee", "AK", "Dheeraj Khatri", "Krishna Tripathi", "Diksha Bharti", "Deepika Goswami", "Shashank Kumar", "Kunal Choudhary", "Vedika", "Abhay Pratap Tiwari", "Kumar Anubhav", "Vinay Dalal", "Bhavishya", "Monty Gautam", "Omshan Sahu", "Krishna Choubey", "Tanvi Kinger", "Vatsalya Joshi", "Harshit Yadav", "Piyush Jha", "Babli Bisht", "Inayetullah Khan", "Ankita Nandi", "Exception Pal", "Baiju Kumar", "Aditya Tiwari", "Ayush", "Shreshtha", "Shivamm Katoch", "Prabhjot Kaur", "Omji Thakur", "Kumaresh Das", "Aditya Deepak Bhandare", "Aaryan Mahto", "Aanya Gupta", "Tanmay Dhok", "Ayush Chandrabanshi", "Abhay Kumar", "Lawanya Kashyap", "Aditya Shrivastava", "Sadashiv Yadav", "Ayush Joshi", "Aryan Saini", "Pratham Mishra", "Sukh", "Tanya Gupta", "Arnav Anand", "Daddy", "Sujal Patil", "Subrata Khan", "Lavish Saini", "Prinkal Kumar", "Ashutosh Kumar", "Pranay Dubey", "Kumar Savan", "Raman Kumar", "Jagrav", "Manjesh", "Divyanshu Sharma", "Saksham Kumar", "Om Sharma", "Kadish Dutta", "Anshuman Chanchar", "Abhinav Kumar Mishra", "Sidharth Tripathy", "Nikhil", "Oshank Sharma", "Uma Sankar Dutta", "Aaishwarya Goel Paswan", "Shreyansh Kumar", "Hanshu", "Aditya Kumar Jha", "Mayank", "Prathamesh", "Aditi S", "Kamalaksha Kanade", "Prince Singh", "Avani Bhat", "Dakshaa Prasad", "Mansi", "Disha", "Arush Mishra", "Rudra Sharma", "Bhavishya Gupta", "Anu Shikha", "Mukul Athwal", "Adrita Ghosh", "Atharva Mehra", "Amiya Alok", "Prakriti", "Siddharth", "Varshitha", "Piyush Kumar", "Aman Meshram", "Ramya Choudhary", "Tanushree Mehrotra", "Divyansh Pratap Singh", "Vijay Humane", "Rajat Yadav", "Swaraj Tayade", "Pragati Tiwari", "Siddhi Bhatia", "Tanishka Chetia", "Nandani Singune", "Aditya", "Dhruv Kr", "Daksh Chandra Pal", "Choudhury S S Palai", "Vraj Talpada", "Kartik Shukla", "Abhinav Anand", "Arth Niteshbhai Zalavadiy", "Trisha Deep Kumar", "Abhishek Kumar", "Ankush", "Lokesh Singh"
+];
+const group205 = [
+    "Shaivya Pandey", "Sneha Das", "Santosh Preksha", "Gunjan Sharma", "Adarsh Kumar Dubey", "Om Hello", "Prisha Kumar", "Yash", "Aditya Kumar Verma", "Anas", "Harmanjit Singh Lubana", "Pushpraj Rawat", "Mohammad Adnan", "Kamayani Lohani", "Yuvraj", "Samarth Class", "Vansh", "Dhruv Nanwani", "Omjit Mitra", "Harkeerat Pruthi", "Sagar Baranwal", "Jayesh Parmar", "Shlok Pandey", "Rajveer", "Piyush Patil", "Soyam Behera", "Aachal Damedher", "Shashwat", "Aditya Chakraborty", "Iit T", "Shaurya Pratap Singh", "Abhinav Kasa", "Its Hrishikesh From Assam", "Avni Agarwal", "Lakshita", "Yuvaraj S K", "Moomin", "Rajwanti Mam", "Divyanshu Priye", "Nandish Ragati", "Shiksha Rathore", "Rajanya Biswas", "Divit Jangir", "Somendra Kumar Shyam", "Gokul", "Aditi Rajendra Dhere", "Shardul Vikram", "Aryan Sharma", "Om Kathar", "Rishabh Yadav", "Kartik Srivastava", "Sakshi Thete", "Raju Chakraborty", "Manshi Bharti", "Jk Jaswal", "Reedip Biswas", "Simran Kumari", "Dhairya Tyagi", "Pawan Papney", "Aditya Raj", "Anay", "Shourya Gupta", "Surya Pratap Singh", "Satyam Kumar", "Dibyanshu Gouda", "Nayan Sonavane", "Harshita Sharma", "Yuvraj", "Adit Maji", "Chintaman Sanap", "Shaivee Chauhan", "Shivansh Gupta", "Ishaan Sonawane", "Mr Genius", "Aim Iitb Ayush", "Shivam Singh", "Keshav Bansal", "Yashasvi", "Sarthak Deval", "Infinite Void", "Vidisha Verma", "Tanmay", "Plaksha Subhkarni", "Sreyansi Das", "Pratham Jain", "Bandana Kumari", "Ayush Kumar Mishra", "Ahir Anand", "Pratham", "Abhinav Yadav", "Rhiddhimaan Paul", "Siddhartha Das", "Naman Mendiratta", "Brajesh Tiwary", "Spandan", "Sriza Sinha Ray", "Ayush Upadhyay", "Susmita Samadder", "Ram Singh", "Ankur Raj", "Om Jagwani", "Prince Verma", "Satyam Dubey", "Rijoy Biswas", "Drashya Patidar", "Neev", "Manasvi Shrivastava", "Kvk Kanishk", "Harshita Pandey", "MPS Apps", "Anwesha Majumder", "Anushtha Singh", "Tannistha Ghosh", "Prateek Kumar Mishra", "Shivansh", "Satyaprakash Yadav", "Bhavnoor Singh Bedi", "Rajdeep Poddar", "Keerthi", "Pulkit Ratta", "Aditya Saxena", "Nehchal Kaur", "Thrisha S Suvarna", "Arya Tarafder", "Ratan Vinnu", "Divyanshu", "Tamanna Rawat", "Naitik Jaiswal"
+];
+const questions = [
+    "What topic did you study today, and how confident do you feel about it?",
+    "How did your last mock test go? What was your score?",
+    "What is your strongest subject, and why?",
+    "How do you manage time during exams? Any tips for others?",
+    "What new concept did you learn this week that you found interesting?",
+    "How many hours did you study today?",
+    "What’s your current average score in practice tests?",
+    "Which subject do you find the most challenging and why?",
+    "How do you stay motivated on tough days?",
+    "Have you made a revision plan for this month? If yes, how does it look?",
+    "What’s your go-to method for relaxing after a long study session?",
+    "How are you managing stress before the upcoming test?",
+    "What was your last mock test score, and how did you feel about it?",
+    "How do you handle difficult problems during practice?",
+    "What’s your strategy for improving weaker areas in your preparation?",
+    "What rank are you aiming for in JEE Advanced?",
+    "How often do you revise concepts, and how effective is your method?",
+    "Which IIT do you dream of joining, and why?",
+    "Have you found any new resources or books that have been helpful for your prep?",
+    "How do you keep track of your progress?",
+    "What is the most important lesson you’ve learned from your preparation so far?",
+    "How do you tackle negative marking during practice tests?",
+    "What’s your current schedule for mock tests and how are you performing in them?",
+    "What’s the toughest question you’ve solved recently, and how did you approach it?",
+    "How do you balance your JEE preparation with other activities or interests?"
+];
+
+function randomInt(max) {
+    return Math.floor(Math.random() * (max ));
+}
+function checkSelected() {
+    const radios = document.getElementsByName('groups'); // Get all radio buttons with name 'options'
+    let selectedValue;
+
+    for (let i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            selectedValue = radios[i].value; // Get the value of the checked radio button
+            break; // Exit the loop once the checked radio button is found
+        }
+    }
+
+    if (selectedValue) {
+            if (selectedValue === "group201") group = group201;
+            else if (selectedValue === "group202") group = group202;
+            else if (selectedValue === "group203") group = group203;
+            else if (selectedValue === "group204") group = group204;
+            else if (selectedValue === "group205") group = group205;
+            let question = questions[randomInt(questions.length)];
+            let student1 = group[randomInt(questions.length)].replace(/\s+/g, '');
+            let student2 = group[randomInt(questions.length)].replace(/\s+/g, '');
+            let student3 = group[randomInt(questions.length)].replace(/\s+/g, '');
+            let displayMessage = "@" + student1 + " @" + student2 + " @" + student3 + " <br> " + question;
+            document.getElementById('output').innerHTML = displayMessage;
+           console.log(displayMessage)
+    } else {
+        console.log('No option selected');
+    }
+}
+function copyToClipboard() {
+    const messageDiv = document.getElementById('output'); // Get the div element
+    const messageText = messageDiv.innerText; // Get the text content of the div
+
+    // Copy the text to the clipboard
+    navigator.clipboard.writeText(messageText).then(() => {
+        alert('Message copied to clipboard!'); // Alert on successful copy
+    }).catch(err => {
+        console.error('Failed to copy: ', err); // Log error if it fails
+    });
+}
+
+
